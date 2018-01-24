670228c085ef8ac3e51011ed464144e80ed4e8ca
Thompson <thompsow@seattleu.edu>
2018-01-24 13:52:45 -0800

    Wed synch

--- f0.bat
+++ f0.bat
@@ -1,4 +1,8 @@
 
 git add -A
-git commit -m "Monday synch"
-git push -u origin master
+git commit -m "Wed synch"
+## git push -u origin master
+## git push https://github.com/wmthompson1/AspNetCoreWebAPI.git
+## to force use -f
+git push -f origin master
+PAUSE
